9a1d6cdc63da54028704fe7a0593a4ee3d47325c
Roman Frolov <frolloff1@yandex.ru>
2023-04-19 17:55:31 +0300

    feat: protect routes with auth

---
+++ middlewares/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// const UnauthorizedError = require('../errors/unauthorized-err');
+
+const { JWT_SECRET, NODE_ENV } = process.env;
+
+module.exports = (req, res, next) => {
+  let token;
+
+  if (req.cookies && req.cookies.jwt) {
+    token = req.cookies.jwt;
+  } else if (
+    req.headers.authorization
+    && req.headers.authorization.startsWith('Bearer ')
+  ) {
+    token = req.headers.authorization.replace('Bearer ', '');
+  } else {
+    return next(Error('Необходима авторизация'));
+  }
+
+  try {
+    const payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
+    );
+    req.user = payload;
+  } catch (error) {
+    return next(Error('Необходима авторизация'));
+  }
+  return next();
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const auth = require('../middlewares/auth');
+
+router.use(auth);
 
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
